3f550a7d577fafc90d8e46e73a9c6d6c195fcd24
Alexander Drummond <113899680+alexanderdrummond@users.noreply.github.com>
2024-03-04 11:23:52 +0100

    Layout, font changes
    - Layout restructured
    - Font changed globally
    - Header added.

---
+++ src/app/components/Static/Header/Links.jsx
@@ -0,0 +1,9 @@
+const Links = () => (
+    <div className="header-links">
+      <a href="#">Forside</a>
+      <a href="#">Sortering</a>
+      <a href="#">Genbrugsstationer</a>
+      <a href="#">Bestil beholder</a>
+    </div>
+  );
+  export default Links;  
